80080423042b52796f0dd4754e8ee381cad4c44d
Tehl <mat_howe@hotmail.com>
2018-04-12 19:06:53 +0100

    Additional node data
    Add all basic plants to node data

--- data/nodes/basic/plants.js
+++ data/nodes/basic/plants.js
@@ -23,5 +23,142 @@ export default [
       }
       //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
     ]
+  },
+  {
+    name: "Root Plants",
+    swings: 1,
+    drops: [
+      {
+        dropTable: [
+          { id: 12161, min: 1, max: 2 }, // Beet
+          { id: 12134, min: 1, max: 2 }, // Carrot
+          { id: 12142, min: 1, max: 2 }, // Onion
+          { id: 12135, min: 1, max: 2 }, // Potato
+          { id: 12162, min: 1, max: 2 }, // Turnip
+          { id: 12329, min: 1, max: 2 } // Yam
+        ],
+        min: 1,
+        max: 1
+      }
+      //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
+    ]
+  },
+  {
+    name: "Vegetable Plants",
+    swings: 1,
+    drops: [
+      {
+        dropTable: [
+          { id: 12332, min: 1, max: 2 }, // Head of Cabbage
+          { id: 12238, min: 1, max: 2 }, // Head of Lettuce
+          { id: 12333, min: 1, max: 2 }, // Kale Leaf
+          { id: 12147, min: 1, max: 2 }, // Mushroom
+          { id: 12241, min: 1, max: 2 }, // Spinach Leaf
+          { id: 12330, min: 1, max: 2 } // Zucchini
+        ],
+        min: 1,
+        max: 1
+      }
+      //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
+    ]
+  },
+  {
+    name: "Omnomberries",
+    swings: 1,
+    drops: [
+      { id: 12128, min: 1, max: 2 }, // Omnomberry
+      {
+        dropTable: [
+          { id: 70426, min: 1, max: 1 }, // Pouch of Black Pigment
+          { id: 74982, min: 1, max: 1 }, // Pouch of Brown Pigment
+          { id: 75270, min: 1, max: 1 }, // Pouch of Orange Pigment
+          { id: 75862, min: 1, max: 1 } // Pouch of White Pigment
+        ],
+        min: 0,
+        max: 1
+      }
+      //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
+    ]
+  },
+  {
+    name: "Snow Truffle",
+    swings: 1,
+    drops: [
+      { id: 12144, min: 1, max: 2 }, // Snow Truffle
+      {
+        dropTable: [
+          { id: 76799, min: 1, max: 1 }, // Pouch of Green Pigment
+          { id: 75862, min: 1, max: 1 } // Pouch of White Pigment
+        ],
+        min: 0,
+        max: 1
+      }
+      //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
+    ]
+  },
+  {
+    name: "Lotus",
+    swings: 1,
+    drops: [
+      { id: 12510, min: 1, max: 2 }, // Lotus Root
+      {
+        dropTable: [
+          { id: 75694, min: 1, max: 1 }, // Pouch of Blue Pigment
+          { id: 71692, min: 1, max: 1 }, // Pouch of Red Pigment
+          { id: 75862, min: 1, max: 1 }, // Pouch of White Pigment
+          { id: 71952, min: 1, max: 1 } // Pouch of Yellow Pigment
+        ],
+        min: 0,
+        max: 1
+      }
+      //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
+    ]
+  },
+  {
+    name: "Orrian Truffle",
+    swings: 1,
+    drops: [
+      { id: 12545, min: 1, max: 2 }, // Orrian Truffle
+      {
+        dropTable: [
+          { id: 77112, min: 1, max: 1 }, // Pouch of Purple Pigment
+          { id: 75862, min: 1, max: 1 } // Pouch of White Pigment
+        ],
+        min: 0,
+        max: 1
+      }
+      //{ id: 19620, min: 0, max: 1 }, // Dandelion Blossom
+    ]
+  },
+  {
+    name: "Flax",
+    swings: 1,
+    drops: [
+      { id: 74090, min: 1, max: 2 }, // Pile of Flax Seeds
+      {
+        dropTable: [
+          { id: 75241, min: 1, max: 1 }, // Flax Fiber
+          { id: 74988, min: 1, max: 1 } // Flax Blossom
+        ],
+        min: 0,
+        max: 1
+      },
+      { id: 9267, min: 0, max: 1 } // Hidden Treasure
+    ]
+  },
+  {
+    name: "Ghost Pepper",
+    swings: 1,
+    drops: [
+      { id: 12544, min: 1, max: 2 }, // Ghost Pepper
+      {
+        dropTable: [
+          { id: 75270, min: 1, max: 1 }, // Pouch of Orange Pigment
+          { id: 71952, min: 1, max: 1 } // Pouch of Yellow Pigment
+        ],
+        min: 0,
+        max: 1
+      }
+    ]
   }
 ];
